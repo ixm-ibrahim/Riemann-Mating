341b101684650c5dcdea5d3e415962059d29b554
Ibrahim Mahmoud <ibrahimibro99@gmail.com>
2020-07-16 02:15:32 -0700

    Updated comments in Game.cs.

--- OpenTK Reimann Mandlebrot/Game.cs
+++ OpenTK Reimann Mandlebrot/Game.cs
@@ -8,12 +8,10 @@ using System.Drawing;
 namespace OpenTK_Reimann_Mating
 {
     /* Problems:
-     *      1. The closer 's' is to zero, the more infinite-points (represented as blue) there are - which obscures the outer 'q' Julia Set
-     *          Note: the blue represtation can be commented out in shader.frag, but this will only replace the blue with some other color - causing the same problem with the rendering
-     *      2. When s is zero, the entire sphere is somehow colored as if it's the outer 'q' Julia Set
-     *      3. When the last n is being displayed (going through all the s for that n), the fractals have slightly different shapes - it's not a smooth transition, even if problems 1 and 2 were not there
-     *          Note: this can be easily seen if the projection is zoomed in, so that q mates into p on the north pole (rather than the original p mating with q on the south pole)
-     * 
+     *      1. The closer 's' is to zero, the more infinite-points there are - which obscures the outer 'q' Julia Set.
+     *      2. When 's' is zero, almost the entirety of the outer 'q' Julia Set is obscured by infinite-points.
+     *      
+     *      Both of these problems are caused by limited GLSL floating-point precision, and can be solved if the push-forward code (in the fragment shader) uses doubles instead of floats. However, this slows down the rendering considerably, and so the ideal would be using floats.
      * 
      * Controls:
      *      WASD, Space, L-Shift: camera movement
